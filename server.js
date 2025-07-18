39e96fd22efcd3f641afe4b2f0eb450316e5794c
Samuel Popoola <samuelpopoola323@gmail.com>
2025-07-18 09:48:23 +0100

    Update server.js

--- server.js
+++ server.js
@@ -13,10 +13,7 @@ app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI);
 .then(() => console.log('✅ MongoDB Connected'))
 .catch(err => console.error('❌ MongoDB Connection Error:', err));
 
